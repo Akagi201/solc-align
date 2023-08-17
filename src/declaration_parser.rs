a1b79f571ee67ae66b14e853abd3b5a426baa5be
Akagi201 <akagi201@gmail.com>
2023-08-17 16:52:35 +0800

    feat: refactor parse engine

--- src/declaration_parser.rs
+++ src/declaration_parser.rs
@@ -1,22 +1,23 @@
 #![allow(unused)]
+
+use crate::utils::{get_full_type, parse_array_size};
 #[derive(Clone, Debug, PartialEq)]
 pub struct Declaration {
     pub storage_type: StorageType,
     pub line: String,
-    pub mod_size: u16, // size mod by 256
-    pub weight: u16,
+    pub mod_size: u32, // size mod by 256
+    pub weight: u32,
 }
 
 /// StorageTypes: ref: https://docs.soliditylang.org/en/latest/types.html
 #[derive(Clone, Debug, PartialEq)]
 pub enum StorageType {
     Bool,
-    Integer(u16),
+    Integer(u32),
     Address,
-    FixedBytes(u16),
-    Bytes,
+    Bytes(u32),
     String,
-    Array(Box<StorageType>),
+    Array(Box<StorageType>, u32),
     Struct,
     Mapping,
     Unknown, // for example, custom struct
@@ -24,33 +25,50 @@ pub enum StorageType {
 
 impl StorageType {
     /// size mod 32
-    pub fn mod_size(&self) -> u16 {
+    pub fn mod_size(&self) -> u32 {
         match self {
             StorageType::Bool => 1,
             StorageType::Integer(n) => (n / 8 - 1) % 32 + 1,
             StorageType::Address => 20,
-            StorageType::FixedBytes(n) => (n - 1) % 32 + 1,
-            StorageType::Bytes => 32,
+            StorageType::Bytes(n) => (n - 1) % 32 + 1,
             StorageType::String => 32,
-            StorageType::Array(t) => (0 + t.mod_size() - 1) % 32 + 1,
+            StorageType::Array(t, n) => (t.mod_size() * n - 1) % 32 + 1,
             StorageType::Struct => 32,
             StorageType::Mapping => 32,
             StorageType::Unknown => 32,
         }
     }
 
+    pub fn from_str(type_name: &str, type_size: u32, array_size: u32) -> Self {
+        if array_size != 0 {
+            return StorageType::Array(
+                Box::new(StorageType::from_str(type_name, type_size, 0)),
+                array_size,
+            );
+        }
+        match type_name {
+            "bool" => StorageType::Bool,
+            "int" | "uint" => StorageType::Integer(type_size),
+            "address" => StorageType::Address,
+            "bytes" => StorageType::Bytes(type_size),
+            "string" => StorageType::String,
+            "mapping" => StorageType::Mapping,
+            _ => StorageType::Unknown,
+        }
+    }
+
     /// order weight order by ascending order
-    pub fn weight(&self) -> u16 {
+    pub fn weight(&self) -> u32 {
         match self {
             StorageType::Unknown => 1,
             StorageType::Struct => 2,
             StorageType::Mapping => 3,
             StorageType::Address => 4,
             StorageType::Integer(256) => 5,
-            StorageType::Bytes => 6,
+            StorageType::Bytes(32) => 6,
             StorageType::String => 7,
-            StorageType::Array(_t) => 8,
-            StorageType::FixedBytes(_n) => 9,
+            StorageType::Array(_t, _n) => 8,
+            StorageType::Bytes(_n) => 9,
             StorageType::Integer(_n) => 10,
             StorageType::Bool => 11,
         }
@@ -59,103 +77,14 @@ impl StorageType {
 
 impl Declaration {
     pub fn from_str(s: &str) -> Declaration {
-        let s = s.trim();
-        if s.starts_with("bool") {
-            let t = StorageType::Bool;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("int") {
-            let w = s.split_whitespace().collect::<Vec<&str>>()[0];
-            let n = w.strip_prefix("int").unwrap();
-            let t: StorageType;
-            if n.trim().len() == 0 {
-                t = StorageType::Integer(256);
-            } else {
-                let n = n.parse::<u16>().unwrap();
-                t = StorageType::Integer(n);
-            }
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("uint") {
-            let w = s.split_whitespace().collect::<Vec<&str>>()[0];
-            let n = w.strip_prefix("uint").unwrap();
-            let t: StorageType;
-            if n.trim().len() == 0 {
-                t = StorageType::Integer(256);
-            } else {
-                let n = n.parse::<u16>().unwrap();
-                t = StorageType::Integer(n);
-            }
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("address") {
-            let t = StorageType::Address;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("bytes") {
-            let w = s.split_whitespace().collect::<Vec<&str>>()[0];
-            let n = w.strip_prefix("bytes").unwrap();
-            let t: StorageType;
-            if n.trim().len() == 0 {
-                t = StorageType::Bytes;
-            } else {
-                let n = n.parse::<u16>().unwrap();
-                t = StorageType::FixedBytes(n);
-            }
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("string") {
-            let t = StorageType::String;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("struct") {
-            let t = StorageType::Struct;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else if s.starts_with("mapping") {
-            let t = StorageType::Mapping;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
-        } else {
-            let t = StorageType::Unknown;
-            Declaration {
-                storage_type: t.clone(),
-                line: s.to_string(),
-                mod_size: t.mod_size(),
-                weight: 0,
-            }
+        let input = get_full_type(s);
+        let (type_name, type_size, array_size) = parse_array_size(input);
+        let storage_type = StorageType::from_str(&type_name, type_size, array_size);
+        Declaration {
+            storage_type: storage_type.clone(),
+            line: s.to_string(),
+            mod_size: storage_type.mod_size(),
+            weight: storage_type.weight(),
         }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,71 @@ pub fn do_vecs_match<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     let matching = a.iter().zip(b).filter(|&(a, b)| a == b).count();
     matching == a.len() && matching == b.len()
 }
+
+pub fn get_full_type(input: &str) -> &str {
+    input.split_whitespace().collect::<Vec<&str>>()[0]
+}
+
+pub fn parse_array_size(input: &str) -> (String, u32, u32) {
+    let mut parts = input.splitn(2, '[');
+    let type_name = parts.next().unwrap().to_string();
+    let remainder = parts.next();
+
+    let (type_name, type_size, array_len) = match remainder {
+        Some(rem) => {
+            let array_size: u32 = rem.trim_end_matches(']').parse().unwrap();
+
+            let (type_name, type_size) = parse_type_size(&type_name);
+            (type_name, type_size, array_size)
+        }
+        None => {
+            let (type_name, type_size) = parse_type_size(&type_name);
+            (type_name, type_size, 0)
+        }
+    };
+
+    (type_name, type_size, array_len)
+}
+
+pub fn parse_type_size(input: &str) -> (String, u32) {
+    let (type_name, type_size) = split_type_and_size(input);
+    match type_name {
+        "bool" => ("bool".to_string(), 1),
+        "address" => ("address".to_string(), 20),
+        "bytes" => {
+            if type_size.is_none() {
+                ("bytes".to_string(), 32)
+            } else {
+                ("bytes".to_string(), type_size.unwrap())
+            }
+        }
+        "string" => ("string".to_string(), 32),
+        "uint" => {
+            if type_size.is_none() {
+                ("uint".to_string(), 32)
+            } else {
+                ("uint".to_string(), type_size.unwrap() / 8)
+            }
+        }
+        "int" => {
+            if type_size.is_none() {
+                ("int".to_string(), 32)
+            } else {
+                ("int".to_string(), type_size.unwrap() / 8)
+            }
+        }
+        _ => (type_name.to_string(), 32),
+    }
+}
+
+pub fn split_type_and_size(input: &str) -> (&str, Option<u32>) {
+    let letter_end = input.chars().position(|c| c.is_digit(10));
+
+    if let Some(letter_end) = letter_end {
+        let (letters, numbers) = input.split_at(letter_end);
+        let number: u32 = numbers.parse().unwrap();
+        (letters, Some(number))
+    } else {
+        (input, None)
+    }
+}
